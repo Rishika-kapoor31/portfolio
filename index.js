9eee448a5cb91b90fe511f44e4456d23c5995525
Rishika Kapoor <60537528+Rishika-kapoor31@users.noreply.github.com>
2021-03-26 20:36:41 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-var i = 0;
-function change() {
-  var doc = document.getElementById("background");
-  var color = ["azure","blue", "brown", "lightgreen"];
-  doc.style.backgroundColor = color[i];
-  i = (i + 1) % color.length;
-}
-setInterval(change, 1000);
